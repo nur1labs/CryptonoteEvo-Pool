054f5b0af7d10cd28345313ed271fa4e76269f4a
Shiro <otakugroup2015@gmail.com>
2018-04-15 22:46:02 +0900

    更新
    更新

---
+++ website/config.js
@@ -0,0 +1,17 @@
+var api = "https://pool.evo.com:9118";
+
+var poolHost = "pool.evo.com";
+
+var irc = "irc.freenode.net/#evo";
+
+var email = "support@evo.com";
+
+var cryptonatorWidget = ["XMR-BTC", "XMR-USD", "BTC-USD"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "https://explorer.evo.com/block/{id}";
+
+var transactionExplorer = "https://explorer.evo.com/tx/{id}";
+
+var themeCss = "themes/default-theme.css";
